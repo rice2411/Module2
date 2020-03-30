a3791d115665f5455ac7636490a82fb0b23202d8
minh <tonthatanhminhktpm@gmail.com>
2020-03-30 16:02:52 +0700

    .

---
+++ .Netcore & C#/netcoreandcshape/tienlai/tienlai.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+namespace tienlai
+{
+    class tienlai
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            double money;
+            int month;
+            double intersetRate;
+            Console.WriteLine("Nhập số tiền gửi: ");
+            money = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Nhập số tháng gửi: ");
+            month = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Nhập lãi suất: ");
+            intersetRate = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Tổng lãi của bạn là: {0}", total(money, month, intersetRate));
+        }
+        public static double total(double mon, int mont, double rat)
+        {
+
+            return mon * (rat / 100) * 12 * mont;
+        }
+    }
+}
