8741123abb3d036c9bdd2f7303ca23695be9b8d1
minh <tonthatanhminhktpm@gmail.com>
2020-03-30 17:10:30 +0700

    .

---
+++ .Netcore & C#/netcoreandcshape/ChiSoCanNang/ChiSoCanNang.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+namespace ChiSoCanNang
+{
+    class ChiSoCanNang
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            double wei, hei;
+            Console.WriteLine("Nhập chiều cao của bạn (m): ");
+            hei = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Nhập cân nặng của bạn (kg): ");
+            wei = Double.Parse(Console.ReadLine());
+            Console.WriteLine(check(wei, hei));
+
+        }
+        public static string check(double weight, double height)
+        {
+            double bmi = weight / (Math.Pow(height, 2));
+            bmi = Math.Round(bmi, 1);
+            if (bmi < 18.5)
+            {
+                return "Thiếu cân";
+            }
+            else if (bmi < 25 && bmi >= 18.5)
+            {
+                return "Bình thường";
+            }
+            else if (bmi < 30 && bmi >= 25)
+            {
+                return "Quá cân";
+            }
+            return "Béo Phì";
+        }
+    }
+}
