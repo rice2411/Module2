6f5a433f0bd33f0011628f43a723bb5ca13c49ae
minh <tonthatanhminhktpm@gmail.com>
2020-03-30 21:17:45 +0700

    .

---
+++ .Netcore & C#/netcoreandcshape/SoNguyenToNhoHon100/SNTnhohon100.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+namespace SoNguyenToNhoHon100
+{
+    class SNTnhohon100
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            int dem = 0;
+            Console.WriteLine("100 số nguyên tố đầu tiên là :");
+            for (int i = 2; i <= 100; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        dem++;
+                    }
+                }
+
+                if (dem == 2)
+                {
+                    Console.WriteLine(i);
+                }
+                dem = 0;
+            }
+        }
+    }
+}
