15d61f97f94578cb12aefd537af15acc6f782384
minh <tonthatanhminhktpm@gmail.com>
2020-03-30 15:14:24 +0700

    .

---
+++ .Netcore & C#/netcoreandcshape/Dientichhinhchnhat/dientichhcn.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+namespace Dientichhinhchnhat
+{
+    class dientichhcn
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            float width, length;
+            Console.WriteLine("Nhập chiều dài: ");
+            width = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Nhập chiều rộng: ");
+            length = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Diện tích hình chữ nhật là: {0}", area(width, length));
+        }
+        public static float area(float wid, float len)
+        {
+            return wid * len;
+        }
+    }
+}
