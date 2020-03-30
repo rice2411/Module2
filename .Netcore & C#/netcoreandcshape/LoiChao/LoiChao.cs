2fdfd37197c5e4cffb7e1dcd8e33fd6a6ac05693
minh <tonthatanhminhktpm@gmail.com>
2020-03-30 17:14:27 +0700

    .

---
+++ .Netcore & C#/netcoreandcshape/LoiChao/LoiChao.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+namespace LoiChao
+{
+    class LoiChao
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            string name;
+            Console.WriteLine("Nhập tên của bạn: ");
+            name = Console.ReadLine();
+            Console.WriteLine("Xin chào {0}", name);
+        }
+    }
+}
