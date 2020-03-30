e42d8b8aa4f66cdb48bc3f4ad67a859cc1d36e00
minh <tonthatanhminhktpm@gmail.com>
2020-03-30 16:54:37 +0700

    .

---
+++ .Netcore & C#/netcoreandcshape/KhaiBaoSuDungBien/KhaiBaoSuDungBien.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+namespace KhaiBaoSuDungBien
+{
+    class KhaiBaoSuDungBien
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            int i = 10;
+            float f = 20.5f;
+            double d = 20.5;
+            Boolean b = true;
+            char c = 'a';
+            String s = "CodeGym";
+            Console.WriteLine("Giá trị của biến i là " + i);
+            Console.WriteLine("Giá trị của biến f là " + f);
+            Console.WriteLine("Giá trị của biến d là " + d);
+            Console.WriteLine("Giá trị của biến b là " + b);
+            Console.WriteLine("Giá trị của biến c là " + c);
+            Console.WriteLine("Giá trị của biến i là " + s);
+        }
+    }
+}
