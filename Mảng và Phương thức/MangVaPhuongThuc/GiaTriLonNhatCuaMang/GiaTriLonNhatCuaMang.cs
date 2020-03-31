db41d15f449fe398df2fd20c0529ca2261227e4e
minh <tonthatanhminhktpm@gmail.com>
2020-03-31 20:04:34 +0700

    .

---
+++ Mảng và Phương thức/MangVaPhuongThuc/GiaTriLonNhatCuaMang/GiaTriLonNhatCuaMang.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+namespace dotnet_FindMax
+{
+    class GiaTriLonNhatCuaMang
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            int size;
+            int[] array;
+            do
+            {
+                Console.WriteLine("Nhập độ dài mảng:");
+                size = Int32.Parse(Console.ReadLine());
+                if (size > 20)
+                    Console.WriteLine("Mảng không được dài quá 20");
+            } while (size > 20);
+
+            array = new int[size];
+            int i = 0;
+            while (i < array.Length)
+            {
+                Console.WriteLine("Nhập giá trị thứ:" + (i + 1) + " : ");
+                array[i] = Int32.Parse(Console.ReadLine());
+                i++;
+            }
+
+            Console.WriteLine("Danh sách các giá trị: ");
+            for (int j = 0; j < array.Length; j++)
+            {
+                Console.WriteLine(array[j] + "\t");
+            }
+
+            int max = array[0];
+            int index = 1;
+            for (int j = 0; j < array.Length; j++)
+            {
+                if (array[j] > max)
+                {
+                    max = array[j];
+                    index = j + 1;
+                }
+            }
+            Console.WriteLine("Giá trị lớn nhất của mảng là " + max + " ,tại vị trí " + index);
+        }
+    }
+}
