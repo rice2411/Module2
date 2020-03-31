e34c34c0fe468a47595e67d02c9bcbea34f5771f
minh <tonthatanhminhktpm@gmail.com>
2020-03-31 10:09:53 +0700

    .

---
+++ .Netcore & C#/netcoreandcshape/ChuyenTien/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+namespace ChuyenTien
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            int money = 0;
+            Console.WriteLine("Nhập số tiền của bạn (USD): ");
+            money = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Mệnh giá VND là: {0} VND", convert(money));
+        }
+        public static int convert(int num)
+        {
+            return num * 23000;
+        }
+    }
+}
