486b42bae9d6a514c3c7192fd7a7a7200cf439ca
minh <tonthatanhminhktpm@gmail.com>
2020-04-05 20:15:18 +0700

    .

---
+++ Access modifier, static method, static property/Access modifier/Accessmodifier.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Access_modifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sum a = new sum();
+            sum2 b = new sum2();
+            Console.WriteLine(a.Sum(2, 2));
+            Console.WriteLine(b.Sum(2, 2));
+
+        }
+        class sum
+        {
+        
+             internal int Sum(int a, int b)
+            {
+                return a + b;
+            }
+        }
+        class sum2
+        {
+     
+            public  int Sum(int a, int b)
+            {
+                return a + b;
+            }
+        }
+    }
+}
