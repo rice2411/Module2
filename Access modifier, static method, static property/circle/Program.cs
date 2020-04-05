b6202fd6794f640d6148e9b3f8a7bc14d24eb9dd
minh <tonthatanhminhktpm@gmail.com>
2020-04-05 22:17:36 +0700

    .

---
+++ Access modifier, static method, static property/circle/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace circle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            circle testcircle = new circle();
+            Console.WriteLine(testcircle.getcolor());
+            Console.WriteLine(testcircle.getraidus());
+        }
+    }
+}
---
+++ Access modifier, static method, static property/circle/circle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace circle
+{
+    class circle
+    {
+        private double RADIUS = 1;
+        private string COLOR ="red";
+        public circle ()
+        {
+            
+        }
+        public double getraidus()
+        {
+            return RADIUS;
+        }
+        public string getcolor()
+        {
+            return COLOR;
+
+        }
+    }
+}
