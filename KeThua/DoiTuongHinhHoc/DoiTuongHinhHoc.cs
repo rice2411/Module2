d8092f52011a6fcc00838279da8ab5cd00406bc1
minh <tonthatanhminhktpm@gmail.com>
2020-04-06 19:55:12 +0700

    .

---
+++ KeThua/DoiTuongHinhHoc/DoiTuongHinhHoc.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace DoiTuongHinhHoc
+{
+    class DoiTuongHinhHoc
+    {
+        static void Main(string[] args)
+        {
+
+            Shape shape = new Shape();
+
+            Console.WriteLine(shape);
+
+
+
+            shape = new Shape("red", false);
+
+            Console.WriteLine(shape);
+
+              Circle circle = new Circle();
+
+                Console.WriteLine(circle);
+
+
+
+                circle = new Circle(3.5);
+
+                Console.WriteLine(circle);
+
+
+
+                circle = new Circle(3.5, "indigo", false);
+
+                Console.WriteLine(circle);
+            Rectangle rectangle = new Rectangle();
+
+            Console.WriteLine(rectangle);
+
+
+
+            rectangle = new Rectangle(2.3, 5.8);
+
+            Console.WriteLine(rectangle);
+
+
+
+            rectangle = new Rectangle(2.5, 3.8, "orange", true);
+
+            Console.WriteLine(rectangle);
+            Square square = new Square();
+
+            Console.WriteLine(square);
+
+
+
+            square = new Square(2.3);
+
+            Console.WriteLine(square);
+
+
+
+            square = new Square(5.8, "yellow", true);
+
+            Console.WriteLine(square);
+        }
+    }
+
+}
