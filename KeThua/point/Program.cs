c0c5beea9341e2504f86b16b2570bb34b2a62983
minh <tonthatanhminhktpm@gmail.com>
2020-04-06 22:53:49 +0700

    .

---
+++ KeThua/point/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace point
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point2d a = new Point2d(1, 1);
+            a.getXY();
+            Point3d b = new Point3d(1, 1, 1);
+            b.getXY();
+;        }
+    }
+}
---
+++ KeThua/point/point3d.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace point
+{
+    class Point3d : Point2d
+    {
+        private float z=0;
+       
+        public Point3d()
+        { }
+        public Point3d(float x, float y, float z) : base(x, y)
+        {
+            this.z=z;
+        }
+        public float getZ()
+        {
+            return this.z;
+        }
+        public void setZ(float z)
+        {
+            this.z =z;
+        }
+        public void getXYZ()
+        {
+            float[] arr = { getX(), getY(), getZ() };
+
+            Console.WriteLine("[{0}]", string.Join(",", arr));
+        }
+        public override string ToString()
+        {
+            return "(" + getX() + "," + getY() + "," + getZ() + ")";
+        }
+    }
+}
