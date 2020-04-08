2363e89608b34e7525c0344c49987fcbe38d0b4a
minh <tonthatanhminhktpm@gmail.com>
2020-04-08 20:30:30 +0700

    .

---
+++ KeThua2/IComparer/Shape.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+
+namespace IComparer
+{
+    public class Shape
+    {
+        private string color = "green";
+        private bool filled = true;
+
+        public Shape()
+        {
+        }
+
+        public Shape(string color, bool filled)
+        {
+            this.color = color;
+            this.filled = filled;
+        }
+
+        public string getColor()
+        {
+            return color;
+        }
+
+        public void setColor(string color)
+        {
+            this.color = color;
+        }
+
+        public bool isFilled()
+        {
+            return filled;
+        }
+
+        public void setFilled(bool filled)
+        {
+            this.filled = filled;
+        }
+
+        public override string ToString()
+        {
+            return "A Shape with color of "
+                    + getColor()
+                    + " and "
+                    + (isFilled() ? "filled" : "not filled");
+        }
+    }
+    public class Circle : Shape
+    {
+        private double radius = 1.0;
+        public Circle()
+        { }
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+        public Circle(double radius, string color, bool filled) : base(color, filled)
+        {
+            this.radius = radius;
+        }
+        public double getRadius()
+        {
+            return this.radius;
+        }
+
+    }
+    public class CircleComparator : IComparer<Circle>
+    {
+        public int Compare(Circle c1, Circle c2)
+        {
+            if (c1.getRadius() > c2.getRadius()) return 1;
+            else if (c1.getRadius() < c2.getRadius()) return -1;
+            else return 0;
+        }
+    }
+}
